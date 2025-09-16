401f9a2272106039ba3f90d24287899461e06b40
thanthtooaung <thanthtoo1285@gmail.com>
2025-09-16 20:14:40 +0700

    :sparkles: feat: final commit

--- account-service/src/main/java/com/example/account/AccountApplication.java
+++ account-service/src/main/java/com/example/account/AccountApplication.java
@@ -1,7 +1,7 @@
 package com.example.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration.class)
 public class AccountApplication {
   public static void main(String[] args){ SpringApplication.run(AccountApplication.class,args); }
 }
